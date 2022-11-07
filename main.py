3ef3e513b0bb10922b64dff747fc3d282488833d
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-11-07 21:59:33 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    img_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('preview', frame)
+    faces = face_cascades.detectMultiScale(img_grey)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('result', frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
